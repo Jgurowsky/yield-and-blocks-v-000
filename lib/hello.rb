e31979d4646e30b41512e02a11402834d077153d
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-07-05 14:28:25 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,10 @@
-def hello_t(names)
-  i = 0
-  while i < names.length
-    yield names[i]
-    i += 1
-  end
-end
+#def hello_t(names)
+  #i = 0
+  #while i < names.length
+    #yield names[i]
+    #i += 1
+  #end
+#end
 
 # call your method here!
 hello_t (["Tim", "Tom", "Jim"]) do |name|
