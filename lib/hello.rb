65143c415f60bb41856c1441ceeac0f68d50872f
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-07-05 14:39:24 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,9 @@
 def hello_t(names)
-  #i = 0
-  #while i < names.length
-    #yield names[i]
-    #i += 1
-  #end
+  i = 0
+  while i < names.length
+    yield names[i]
+    i += 1
+  end
 end
 
 # call your method here!
